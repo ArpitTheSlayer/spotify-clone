c1fdebabdd428999a6d02ea80d25201e89a1c3f1
ArpitTheSlayer <arpitpandey.2001@gmail.com>
2025-06-20 07:07:24 +0530

    Implements basic song playback functionality
    Adds basic song playback functionality, including play/pause, song information display, and time elapsed/duration display.
    
    The changes introduce a new `Audio` object to manage the current song and update the UI accordingly.
    A new `songPlayPauseFunction` toggles the play/pause state of the current song.
    The song information and time are updated as the song plays.

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 const songsElement = document.querySelector(".songs");
-let currentSong;
+const songInfoElement = document.querySelector(".song-info");
+const songTimeElement = document.querySelector(".song-time");
+const playButton = document.querySelector(".play-button");
+const songs = [];
+let currentSong = new Audio();
 
 const getSongs = async () => {
   const link = await fetch("http://127.0.0.1:3000/spotify-clone/songs/");
@@ -7,7 +11,6 @@ const getSongs = async () => {
   const songsPage = document.createElement("div");
   songsPage.innerHTML = response;
   const songsPageAnchorTags = Array.from(songsPage.querySelectorAll("a"));
-  const songs = [];
   songsPageAnchorTags.forEach((anchorTag) => {
     if (anchorTag.href.endsWith(".mp3")) songs.push(anchorTag);
   });
@@ -74,20 +77,79 @@ const getSongs = async () => {
           </div>`;
     const songPlayButton = songElement.querySelector(".play-buttons");
     songPlayButton.addEventListener("click", () => {
-      playSong(song.href);
+      currentSong.src = song.href;
+      songPlayPauseFunction();
+      currentSong.play();
+      songInfoElement.innerHTML = song.innerHTML.replace(".mp3", "");
+      songTimeElement.innerHTML = "00:00 / 00:00";
     });
     songsElement.append(songElement);
   });
+  if (currentSong.src === "") {
+    currentSong.src = songs[0].href;
+    songInfoElement.innerHTML = songs[0].innerHTML.replace(".mp3", "");
+    songTimeElement.innerHTML = "00:00 / 00:00";
+  }
+};
+
+const songPlayPauseFunction = () => {
+  if (currentSong.paused) {
+    currentSong.play();
+    playButton.innerHTML = `<svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 24 24"
+          class="stroke-icon"
+        >
+          <circle
+            cx="12"
+            cy="12"
+            r="10"
+            stroke="currentColor"
+            stroke-width="1.5"
+          />
+          <path
+            d="M9.5 9L9.5 15M14.5 9V15"
+            stroke="currentColor"
+            stroke-width="1.5"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+          />
+        </svg>`;
+  } else {
+    currentSong.pause();
+    playButton.innerHTML = `<svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+              class="stroke-icon"
+            >
+              <circle
+                cx="12"
+                cy="12"
+                r="10"
+                stroke="currentColor"
+                stroke-width="1.5"
+              />
+              <path
+                d="M9.5 11.1998V12.8002C9.5 14.3195 9.5 15.0791 9.95576 15.3862C10.4115 15.6932 11.0348 15.3535 12.2815 14.6741L13.7497 13.8738C15.2499 13.0562 16 12.6474 16 12C16 11.3526 15.2499 10.9438 13.7497 10.1262L12.2815 9.32594C11.0348 8.6465 10.4115 8.30678 9.95576 8.61382C9.5 8.92086 9.5 9.6805 9.5 11.1998Z"
+                fill="currentColor"
+              />
+            </svg>`;
+  }
+};
+
+const secondsToDuration = (time) => {
+  const minutes = Math.floor(time / 60);
+  const seconds = Math.floor(time % 60);
+  // TODO: add leading zero
+  return `${minutes.toString().padStart(2, "0")}:${seconds}`;
 };
 
 getSongs();
 
-const playButton = document.querySelector(".play-button");
+playButton.addEventListener("click", songPlayPauseFunction);
 
-const playSong = async (currentSong) => {
-  let audio = new Audio(currentSong);
-  playButton.addEventListener("click", () => {
-    audio.play();
-    console.log("here");
-  });
-};
+currentSong.addEventListener("timeupdate", () => {
+  const currentTime = secondsToDuration(currentSong.currentTime);
+  const currentDuration = secondsToDuration(currentSong.duration);
+  songTimeElement.innerHTML = `${currentTime} / ${currentDuration}`;
+});
